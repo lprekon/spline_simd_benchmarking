947f2baa6d126bc9b5524b3b4e002b9225460586
EC2 Default User <ec2-user@ip-172-31-22-254.ec2.internal>
2025-02-14 17:48:49 +0000

    moving files over

---
+++ benches/benchmarking.rs
@@ -0,0 +1,35 @@
+#![feature(test)]
+extern crate test;
+use test::Bencher;
+
+use rust_simd_becnhmarking::{b_spline, b_spline_loop_over_basis};
+
+const DEGREE: usize = 4;
+const CONTROL_POINTS: [f64; 16] = [1.0; 16];
+const KNOTS: [f64; 21] = [
+    0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0,
+    17.0, 18.0, 19.0, 20.0,
+];
+
+const INPUT_SIZE: usize = 100;
+
+#[bench]
+/// benchmark evaluating a degree-3 B-spline with 20 knots and 16 basis functions, over 100 different input values
+fn bench_recursive_method(b: &mut Bencher) {
+    let input_values: Vec<f64> = (0..INPUT_SIZE).map(|x| x as f64 / 10.0).collect(); // 100 input values, ranging from 0.0 to 9.9
+    b.iter(|| {
+        for x in input_values.iter() {
+            let _ = b_spline(*x, &CONTROL_POINTS, &KNOTS, DEGREE);
+        }
+    });
+}
+
+#[bench]
+fn bench_simple_loop_method(b: &mut Bencher) {
+    let input_values: Vec<f64> = (0..INPUT_SIZE).map(|x| x as f64 / 10.0).collect(); // 100 input values, ranging from 0.0 to 9.9
+    b.iter(|| {
+        for x in input_values.iter() {
+            let _ = b_spline_loop_over_basis(*x, &CONTROL_POINTS, &KNOTS, DEGREE);
+        }
+    });
+}
---
+++ src/lib.rs
@@ -0,0 +1,63 @@
+/// recursivly compute the b-spline basis function for the given index `i`, degree `k`, and knot vector, at the given parameter `x`
+pub fn basis_activation(i: usize, k: usize, x: f64, knots: &[f64]) -> f64 {
+    if k == 0 {
+        if knots[i] <= x && x < knots[i + 1] {
+            return 1.0;
+        } else {
+            return 0.0;
+        }
+    }
+    let left_coefficient = (x - knots[i]) / (knots[i + k] - knots[i]);
+    let left_recursion = basis_activation(i, k - 1, x, knots);
+
+    let right_coefficient = (knots[i + k + 1] - x) / (knots[i + k + 1] - knots[i + 1]);
+    let right_recursion = basis_activation(i + 1, k - 1, x, knots);
+
+    let result = left_coefficient * left_recursion + right_coefficient * right_recursion;
+    return result;
+}
+
+/// Calculate the value of the B-spline at the given parameter `x`
+pub fn b_spline(x: f64, control_points: &[f64], knots: &[f64], degree: usize) -> f64 {
+    let mut result = 0.0;
+    for i in 0..control_points.len() {
+        result += control_points[i] * basis_activation(i, degree, x, knots);
+    }
+    return result;
+}
+
+pub fn b_spline_loop_over_basis(
+    x: f64,
+    control_points: &[f64],
+    knots: &[f64],
+    degree: usize,
+) -> f64 {
+    let mut basis_activations = vec![0.0; knots.len() - 1];
+    // fill the basis activations vec with the valued of the degree-0 basis functions
+    for i in 0..knots.len() - 1 {
+        if knots[i] <= x && x < knots[i + 1] {
+            basis_activations[i] = 1.0;
+        } else {
+            basis_activations[i] = 0.0;
+        }
+    }
+
+    for k in 1..=degree {
+        for i in 0..knots.len() - k - 1 {
+            let left_coefficient = (x - knots[i]) / (knots[i + k] - knots[i]);
+            let left_recursion = basis_activations[i];
+
+            let right_coefficient = (knots[i + k + 1] - x) / (knots[i + k + 1] - knots[i + 1]);
+            let right_recursion = basis_activations[i + 1];
+
+            basis_activations[i] =
+                left_coefficient * left_recursion + right_coefficient * right_recursion;
+        }
+    }
+
+    let mut result = 0.0;
+    for i in 0..control_points.len() {
+        result += control_points[i] * basis_activations[i];
+    }
+    return result;
+}
