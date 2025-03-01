33b7d647cee86e3c63758addf12a456f98947183
EC2 Default User <ec2-user@ip-172-31-22-254.ec2.internal>
2025-03-01 04:17:53 +0000

    x86 instrinsic code added. untested

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 #![feature(portable_simd)]
-
+#![feature(stdarch_x86_avx512)]
 use std::vec;
 
 /// recursivly compute the b-spline basis function for the given index `i`, degree `k`, and knot vector, at the given parameter `x`
@@ -286,6 +286,123 @@ pub fn b_spline_portable_simd_transpose(
         while i < control_points.len() {
             let basis_index = i * inputs.len() + x_idx;
             result += control_points[i] * basis_activations[basis_index];
+            i += 1;
+        }
+        outputs.push(result);
+    }
+
+    return outputs;
+}
+
+#[cfg(all(
+    target_arch = "x86_64",
+    // target_feature = "sse2",
+    // target_feature = "avx512f",
+))]
+pub fn b_spline_x86_intrinsics(
+    inputs: &[f64],
+    control_points: &[f64],
+    knots: &[f64],
+    degree: usize,
+) -> Vec<f64> {
+    use std::arch::x86_64::*;
+    let mut outputs = Vec::with_capacity(inputs.len());
+    let mut basis_activations = vec![0.0; knots.len() - 1];
+    for x in inputs {
+        let x_splat = unsafe { _mm512_set1_pd(*x) };
+
+        let mut i = 0;
+        // SIMD step for the degree-0 basis functions
+        while i + SIMD_WIDTH < knots.len() - 1 {
+            unsafe {
+                let knots_i_vec = _mm512_load_pd(&knots[i]);
+                let knots_i_plus_1_vec = _mm512_load_pd(&knots[i + 1]);
+
+                let left_mask = _mm512_cmp_pd_mask(knots_i_vec, x_splat, _CMP_LE_OQ);
+                let right_mask = _mm512_cmp_pd_mask(x_splat, knots_i_plus_1_vec, _CMP_LT_OQ);
+                let full_mask = left_mask & right_mask;
+                let activation_vec =
+                    _mm512_mask_blend_pd(full_mask, _mm512_set1_pd(1.0), _mm512_set1_pd(0.0));
+
+                _mm512_store_pd(&mut basis_activations[i], activation_vec);
+            }
+            i += SIMD_WIDTH;
+        }
+        // scalar step for the degree-0 basis functions
+        while i < knots.len() - 1 {
+            if knots[i] <= *x && *x < knots[i + 1] {
+                basis_activations[i] = 1.0;
+            } else {
+                basis_activations[i] = 0.0;
+            }
+            i += 1;
+        }
+
+        for k in 1..=degree {
+            let mut i = 0;
+            // SIMD step for the higher degree basis functions
+            while i + SIMD_WIDTH < knots.len() - k - 1 {
+                unsafe {
+                    let knots_i_vec = _mm512_load_pd(&knots[i]);
+                    let knots_i_plus_1_vec = _mm512_load_pd(&knots[i + 1]);
+                    let knots_i_plus_k_vec = _mm512_load_pd(&knots[i + k]);
+                    let knots_i_plus_k_plus_1_vec = _mm512_load_pd(&knots[i + k + 1]);
+
+                    let left_cofficient_numerator_vec = _mm512_sub_pd(x_splat, knots_i_vec);
+                    let left_cofficient_denominator_vec =
+                        _mm512_sub_pd(knots_i_plus_k_vec, knots_i_vec);
+                    let left_cofficient_vec = _mm512_div_pd(
+                        left_cofficient_numerator_vec,
+                        left_cofficient_denominator_vec,
+                    );
+                    let left_recursion_vec = _mm512_load_pd(&basis_activations[i]);
+
+                    let right_cofficient_numerator_vec =
+                        _mm512_sub_pd(knots_i_plus_k_plus_1_vec, x_splat);
+                    let right_cofficient_denominator_vec =
+                        _mm512_sub_pd(knots_i_plus_k_plus_1_vec, knots_i_plus_1_vec);
+                    let right_cofficient_vec = _mm512_div_pd(
+                        right_cofficient_numerator_vec,
+                        right_cofficient_denominator_vec,
+                    );
+                    let right_recursion_vec = _mm512_load_pd(&basis_activations[i + 1]);
+
+                    let left_val_vec = _mm512_mul_pd(left_cofficient_vec, left_recursion_vec);
+                    let right_val_vec = _mm512_mul_pd(right_cofficient_vec, right_recursion_vec);
+
+                    let new_basis_activations_vec = _mm512_add_pd(left_val_vec, right_val_vec);
+                    _mm512_store_pd(&mut basis_activations[i], new_basis_activations_vec);
+                }
+                i += SIMD_WIDTH;
+            }
+            // scalar step for the higher degree basis functions
+            while i < knots.len() - k - 1 {
+                let left_coefficient = (x - knots[i]) / (knots[i + k] - knots[i]);
+                let left_recursion = basis_activations[i];
+
+                let right_coefficient = (knots[i + k + 1] - x) / (knots[i + k + 1] - knots[i + 1]);
+                let right_recursion = basis_activations[i + 1];
+
+                basis_activations[i] =
+                    left_coefficient * left_recursion + right_coefficient * right_recursion;
+                i += 1;
+            }
+        }
+
+        // SIMD step for the final result
+        let mut i = 0;
+        let mut result = 0.0;
+        while i + SIMD_WIDTH < control_points.len() {
+            unsafe {
+                let control_points_vec = _mm512_load_pd(&control_points[i]);
+                let basis_activations_vec = _mm512_load_pd(&basis_activations[i]);
+                let result_vec = _mm512_mul_pd(control_points_vec, basis_activations_vec);
+                result += _mm512_reduce_add_pd(result_vec);
+            }
+            i += SIMD_WIDTH;
+        }
+        while i < control_points.len() {
+            result += control_points[i] * basis_activations[i];
             i += 1;
         }
         outputs.push(result);
